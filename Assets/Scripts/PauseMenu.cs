912cc78eddda928412280cf130592785e6aedea8
DESKTOP-PS2G88H\Eduardo <eduardoandreoni2010@gmail.com>
2021-06-21 15:00:16 -0300

    Menu de Pause no Leveu 1

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public void StageSelect()
+    {
+        //SceneManager.LoadScene("StageSelect");
+        Debug.Log("Seleção de Fases");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+
+    }
+
+
+
+
+}
