5a5224721d52f2afa7dc7bdc527d9e942ed37aad
DESKTOP-PS2G88H\Eduardo <eduardoandreoni2010@gmail.com>
2021-07-12 14:58:18 -0300

    Game Maneger e Menu de Pause.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,12 +4,32 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject pauseMenu;
+    public GameObject optionMenu;
+
+    void Awake() 
+    {
+        //pauseMenu = GameObject.Find("Menu Pause");
+
+        //optionMenu = GameObject.Find("Menu Options");
+    }
+
     public void Pause()
     {
         Time.timeScale = 0;
+
+        pauseMenu.SetActive(true);
     }
     public void Despause()
     {
+        pauseMenu.SetActive(false);
+
         Time.timeScale = 1;
+
+        optionMenu.SetActive(false);
+
     }
-}   
+
+}
+
+
