d7cdd60724fb0c4804a794bdfc3285f0f6e5ca76
DESKTOP-PS2G88H\Eduardo <eduardoandreoni2010@gmail.com>
2021-08-09 15:04:15 -0300

    Sistema de Seleção de fases

---
+++ Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class LevelSelect : MonoBehaviour
+{
+    public List<GameObject> fases;
+
+    private GameObject selected;
+
+    void Start()
+    {
+        //Pega todos os filhos dos objetos com os botões de fases e adiciona a lista de fases
+        foreach (Transform child in transform)
+        {
+            fases.Add(child.gameObject);
+        }
+        
+    }
+
+    public void LevelChange()
+    {
+        selected = EventSystem.current.currentSelectedGameObject;
+
+        SceneManager.LoadScene(fases.IndexOf(selected.gameObject) + 2);
+    }
+
+
+}
